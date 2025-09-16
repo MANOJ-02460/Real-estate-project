cffc91a3b589f011785fa02ec55d688f174b6c6b
MANOJ <rajarapumanoj1999@gmail.com>
2025-09-16 16:18:44 +0530

    Real-estate-project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <Navbar/>
         <div className='intro'>
             <p>Looking for a Property!</p>
-            <h1><span>Buy</span>and<span>Sell</span></h1>
+            <h1><span>Buy</span>&nbsp;and&nbsp;<span>Sell</span></h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, fugiat obcaecati laborum sunt distinctio molestiae. Dicta mollitia debitis aut. Nostrum.</p>
             <a href='#' className='header-btn'>Details</a>
         </div>
