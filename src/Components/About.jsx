52368d1fdc09a355cfb5943d7168dc5256d65126
MANOJ <rajarapumanoj1999@gmail.com>
2025-09-17 21:19:14 +0530

    Real-estate-project

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import aboutImage from '../Images/About.png'
 
 const About = () => {
   return (
     <div className='about' id="about">
         <Navbar/>
         <div className='about-model'>
-            <img src='https://ucb5a172c022409aed896dba53d4.previews.dropboxusercontent.com/p/thumb/ACzZyd_cI7pcfLfvSss22ONhsKBHJVw6yiGFmxxTVaDQn4PO3ZyGxB9ztKP2E2QEk54eHOztMeqGqfwBvqIWUa0dA1lx9Hib5Z3yLmpEhjiyTddKSwAvZDuVBRMcKGnSrdmu3-FHMpUPeRVY9SSFs_hTfox5YCLZ3L4zWx0ho-c9R5_Z7J8DiuP8IHCpfAL8Hmrn3EXUDyeAGZYTLHFiatifo0uL6KHxiK67BeVwQJRchvNftwTBuDi5B2Vz_opxqsWj4Z00lON9P2ucEAzwSBUj-QSZC5XJ6kJePiee27YtkOKBccFfY8prfmM3eYJ-CeqLJSUCUchHma0sA5ygLsEUZgMjVbANTq0Kb7b3qGczUQ/p.png' alt='about-image'/>
+            <img src={aboutImage} alt='about-image'/>
         </div>
         <div className='about-text'>
             <h2>We Are The Best <br/> Real Estate Company</h2>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import {Link} from 'react-router-dom'
 
+import logo from '../Images/Logo.png'
+
 const navbar = [
   {name:"Home", path:"#home"},
   {name:"About", path:"#about"},
@@ -13,7 +15,7 @@ const Navbar = () => {
   return (
     <nav>
         <a href='#' className='logo'>
-            <img src='https://uc7ee4221d7e828bbc2ae941254b.previews.dropboxusercontent.com/p/thumb/ACy8QXqHG0N-5ovm7mhLuJEvGD8VbzL3vDZPdOEH8vrxpgNp8e6FY0PddT_F5vzdP76tnAtYVKFhwxpdnwb-UP0_sl2fZh_RunsAgK4RIFWvncsRaCuKxIp0A_4qNd9bgajZo8gkAyYUOCaDZPdFlQiga7pUt-GywyEMXbb_Qn3wSBo-cxTB-vhTmkijoRpGAsB_Pbzlh_RrVx0whtAG6dQRravzo92a_rT09s3BjHgjKzA7_LnVBJOFDk8PrFN8PCpPZzl0cV8p2PlBj4cET43SMDwhb5yekHEV93GS1KpfClFIjynOCuN6Y0Nn_J3MCxtmYt0-eG0IDnZO1xbhjeL7/p.png' alt='Logo '/>
+            <img src={logo} alt='Logo '/>
         </a>
         <input className='menu-btn' type='checkbox' id='menu-btn'/>
         <label className='menu-icon' for='menu-btn'>
